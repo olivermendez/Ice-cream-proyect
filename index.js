89403150d7b44a1f8ac07371da2e4bc9f01e9b6a
Oliver Mendez <olivermendezpersonal9@gmail.com>
2019-10-30 05:17:57 -0400

    Se instalo el plugin Inert y se descargo una plantilla html para poder visualizar mejor el trabajo que se va a realizar.

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
-'use strict'
+'use strict' 
 
 //Requerir el modulo de hapi (Framework)
 const Hapi = require('hapi');
 
+//Framework para dar respuesta a archivos.
+const inert = require('inert');
+
+const path = require('path');
+
 //Se configura el servidor de la app.
 const server = Hapi.Server({
     port: process.env.port || 3000,
-    host:'localhost'
+    host:'localhost',
+    routes:{
+        files:{
+            relativeTo:path.join(__dirname,'public')
+        }
+    }
 });
 
 //Se define la funcion para inicializar el proyecto.
 async function init(){
     
-
-    //En esta parte se define las rutas
-    //>Metodo http, url, controlador de ruta
-    server.route({
-
-        //Metodo http
-        method: 'GET',
-
-        //url
-        path:'/',
-
-        //controlador de la ruta
-        handler:(req,h) => {
-
-            //El objeto <h> trae consigo un conjunto de respuestas
-            return h.response('hola mundo').code(200);
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path:'/redirect',
-        handler:(req,h) => {
-
-            //El objeto <h> trae consigo un conjunto de respuestas
-            return h.redirect('https://platzi.com');
-        }
-    });
+    
 
     //Al arrancar el servidor, HapiJs, se considera una tarea asincrona
     //Cuando trabajamos con procesos asincronos, debemos incluir un <await> y luego validar posibles erroes con el <try/catch>
     
     try{
+        await server.register(inert);
+
+        //definiendo las rutas
+        server.route({
+            method: 'GET', //Metodo http
+            path:'/home', //url
+        
+        //controlador de la ruta
+            handler:(req,h) => {
+            return h.file('index.html');
+            //El objeto <h> trae consigo un conjunto de respuestas
+        }
+    });
+
+        server.route({
+            method: 'GET',
+            path:'/{param*}',
+            handler:{
+                directory:{
+                    path:'.',
+                    index: ['index.html']
+                }
+
+
+            //El objeto <h> trae consigo un conjunto de respuestas
+            
+            }
+    });
+
+
         
         await server.start();
 
